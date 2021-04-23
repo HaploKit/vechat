0737fc1d1d0dbe2228c1da0b8f7c4022a786d050
Xiao <vincent@cwi.nl>
2021-04-23 21:37:10 +0200

    update

--- src/window.cpp
+++ src/window.cpp
@@ -284,7 +284,8 @@ namespace racon
         largestsubgraph = graph.LargestSubgraph();
 
         //cause bug if largestsubgraph is too smaller than the original one
-        if (largestsubgraph.nodes().size() / graph.nodes().size() < 0.5)
+        if (largestsubgraph.nodes().size() <200)
+        // if (largestsubgraph.nodes().size() / graph.nodes().size() < 0.5)
         {
             std::cerr << "This is very bad:" << largestsubgraph.nodes().size()<<"\t"<<
             graph.nodes().size() << "\n";
