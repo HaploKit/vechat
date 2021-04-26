3c64b90ce66db891918a34af05dd73ab2268102a
Xiao <vincent@cwi.nl>
2021-04-26 21:09:14 +0200

    update

--- src/main.cpp
+++ src/main.cpp
@@ -63,7 +63,7 @@ int main(int argc, char** argv) {
     bool haplotype = false;
     double min_confidence=0.22;
     double min_support=0.19;
-    std::uint32_t num_prune = 3;
+    uint32_t num_prune = 3;
     uint32_t num_threads = 1;
 
     uint32_t cudapoa_batches = 0;
@@ -71,7 +71,7 @@ int main(int argc, char** argv) {
     uint32_t cudaaligner_band_width = 0;
     bool cuda_banded_alignment = false;
 
-    std::string optstring = "ufpdskw:q:e:m:x:g:t:h";
+    std::string optstring = "ufp:d:s:k:w:q:e:m:x:g:t:h";
 #ifdef CUDA_ENABLED
     optstring += "bc::";
 #endif
