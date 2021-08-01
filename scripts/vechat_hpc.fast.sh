61f7de8f6051022f84bdf601a391954a5f6b2856
Xiao <vincent@cwi.nl>
2021-08-01 11:49:54 +0200

    update

--- scripts/vechat_hpc.fast.sh
+++ scripts/vechat_hpc.fast.sh
@@ -59,7 +59,8 @@ do
     overlap=$target_read.paf
     echo -n "$binpath/minimap2 -cx ava-$platform --dual=yes $target_read $outdir/reads.round1.fa -t $threads |awk '\$11>=1000 && \$10/\$11>=0.99' |cut -f 1-12|$binpath/fpa drop --same-name --internalmatch  - > $overlap; "
     # echo -n "$hapracon -f  -t $threads  $outdir/reads.round1.fa   $overlap  $target_read >$target_read.corrected.tmp;"
-    echo -n "/vol/biotools/bin/racon -f  -t $threads  $outdir/reads.round1.fa   $overlap  $target_read >$target_read.corrected.tmp;" #use racon installed by administrator to run on HPC
+    # echo -n "/vol/biotools/bin/racon -f  -t $threads  $outdir/reads.round1.fa   $overlap  $target_read >$target_read.corrected.tmp;" #too old racon
+    echo -n "/prj/whatshap-denovo/software/miniconda3/bin/racon -f  -t $threads  $outdir/reads.round1.fa   $overlap  $target_read >$target_read.corrected.tmp;" #use racon installed by conda to run on HPC
 
     echo -n "$SCRIPTDIR/filter_fa $target_read.corrected.tmp $min_corrected_len >$target_read.corrected2.fa; "
     # echo "rm -f $overlap $target_read.corrected.tmp;"
