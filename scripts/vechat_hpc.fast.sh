0539421d2f5ffc494e66b729ff679d3cf173a145
Xiao <vincent@cwi.nl>
2021-08-01 10:00:05 +0200

    update

--- scripts/vechat_hpc.fast.sh
+++ scripts/vechat_hpc.fast.sh
@@ -30,7 +30,7 @@ split -l $n_lines -d --additional-suffix .tmp.fq $raw_read $outdir/reads_chunk
 for target_read in $outdir/reads_chunk*.tmp.fq
 do
     overlap=$target_read.paf
-    echo -n "$binpath/minimap2 -x ava-$platform --dual=yes $raw_read $target_read -t $threads |awk '\$11>=500' |$binpath/fpa drop --same-name --internalmatch  - > $overlap; "
+    echo -n "$binpath/minimap2 -x ava-$platform --dual=yes  $target_read $raw_read -t $threads |awk '\$11>=500' |$binpath/fpa drop --same-name --internalmatch  - > $overlap; "
     echo -n "$hapracon -f -p -d $min_confidence -s $min_support -t $threads  $raw_read  $overlap  $target_read >$target_read.corrected.tmp.fa;"
     echo -n "$SCRIPTDIR/filter_fa $target_read.corrected.tmp.fa $min_corrected_len >$target_read.corrected.fa; "
     # echo "rm -f $overlap $target_read $target_read.corrected.tmp.fa;"
@@ -44,7 +44,10 @@ for i in `ls sub-1r*`;do qsub -cwd -P fair_share -S /bin/bash -l arch=lx-amd64 -
 
 #check if finished successfully manually !!!
 
-#run the second round 
+#####################################
+######## run the second round #######
+#####################################
+
 #Note that in the second round, it will lost many overlaps if splitting reads when running minimap2.
 for i in $outdir/reads_chunk*corrected.fa
 do
@@ -54,7 +57,7 @@ done >$outdir/reads.round1.fa
 for target_read in $outdir/reads_chunk*corrected.fa
 do
     overlap=$target_read.paf
-    echo -n "$binpath/minimap2 -cx ava-$platform --dual=yes $outdir/reads.round1.fa $target_read -t $threads |awk '\$11>=1000 && \$10/\$11>=0.99' |cut -f 1-12|$binpath/fpa drop --same-name --internalmatch  - > $overlap; "
+    echo -n "$binpath/minimap2 -cx ava-$platform --dual=yes  $target_read $outdir/reads.round1.fa -t $threads |awk '\$11>=1000 && \$10/\$11>=0.99' |cut -f 1-12|$binpath/fpa drop --same-name --internalmatch  - > $overlap; "
     # echo -n "$hapracon -f  -t $threads  $outdir/reads.round1.fa   $overlap  $target_read >$target_read.corrected.tmp;"
     echo -n "/vol/biotools/bin/racon -f  -t $threads  $outdir/reads.round1.fa   $overlap  $target_read >$target_read.corrected.tmp;" #use racon installed by administrator
 
